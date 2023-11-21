47f0c84127dd96a2261c5d90337c0a2f0a962db2
minix <minix@minix>
2023-11-21 19:17:39 +0700

    Add Section 2

--- app/src/main/java/com/minix/diaryapp/MainActivity.kt
+++ app/src/main/java/com/minix/diaryapp/MainActivity.kt
@@ -3,13 +3,21 @@ package com.minix.diaryapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
+import com.minix.diaryapp.navigation.SetupNavigationGraph
 import com.minix.diaryapp.ui.theme.DiaryAppTheme
+import com.minix.diaryapp.navigation.Screen
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             DiaryAppTheme {
+                val navController = rememberNavController()
+                SetupNavigationGraph(startDestination = Screen.Authentication.route, navController = navController)
             }
         }
     }
